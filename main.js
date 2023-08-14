2402fe6b517b3ade262abba8bc0137419c4b405c
Surbhi Dhage <84434180+surbhidhage@users.noreply.github.com>
2023-08-14 19:48:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+/*Login section*/
+
+function showMenu() {
+  var x = document.getElementById("myMenu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+/*Map section*/
+
+function showMap() {
+  var x = document.getElementById("myMap");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
